bb7c3352a55a84ed67ccf7984766e58bc923de16
Ершов Андрей <rutikeyone@gmail.com>
2025-03-01 21:47:29 +0300

    The example has been completed

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,54 @@
 void main() {
   final order = Order("12345", 250.0);
-  order.createOrder();
+  final orderRepository = OrderRepository();
+  final notificationService = NotificationService();
+  final logger = Logger();
+  final orderManager = OrderManager(orderRepository, notificationService, logger);
+
+  orderManager.processOrder(order);
 }
+
 
 class Order {
   final String orderId;
   final double amount;
 
-  Order(this.orderId, this.amount);
+  const Order(this.orderId, this.amount);
+}
 
-  void createOrder() {
-    print("Order $orderId created with amount \$$amount");
-    processPayment();
-    sendOrderConfirmation();
+class OrderRepository {
+  void createOrder(Order order) {
+    print("Order ${order.orderId} created with amount \$${order.amount}");
   }
 
-  void processPayment() {
-    print("Processing payment for order $orderId with amount \$$amount");
-  }
-
-  void sendOrderConfirmation() {
-    print("Sending confirmation for order $orderId");
+  void saveOrderToDatabase(Order order) {
+    print("Saving order ${order.orderId} to database");
   }
 }
 
+class NotificationService {
+  void sendOrderConfirmation(Order order) {
+    print("Sending confirmation for order ${order.orderId}");
+  }
+}
+
+class Logger {
+  void logOrderCreation(Order order) {
+    print("Logging order ${order.orderId} creation");
+  }
+}
+
+class OrderManager {
+  final OrderRepository _orderRepository;
+  final NotificationService _notificationService;
+  final Logger _logger;
+
+  OrderManager(this._orderRepository, this._notificationService, this._logger);
+
+  void processOrder(Order order) {
+    _orderRepository.createOrder(order);
+    _orderRepository.saveOrderToDatabase(order);
+    _notificationService.sendOrderConfirmation(order);
+    _logger.logOrderCreation(order);
+  }
+}
