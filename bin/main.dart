f8c680b4b37451743b567fef9cb41ed301632417
Ершов Андрей <rutikeyone@gmail.com>
2025-03-01 21:58:31 +0300

    The language for comments has been changed from English to Russian

--- bin/main.dart
+++ bin/main.dart
@@ -10,21 +10,21 @@ class Order {
   Order(this.orderId, this.amount);
 
   void createOrder() {
-    print("Order $orderId created with amount \$$amount");
+    print("Заказ с идентификатором $orderId был создан на сумму \$$amount");
     _saveOrderToDatabase();
     _sendOrderConfirmation();
     _logOrderCreation();
   }
 
   void _saveOrderToDatabase() {
-    print("Saving order $orderId to database");
+    print("Сохранение данных о заказе с идентификатором $orderId в базу данных");
   }
 
   void _sendOrderConfirmation() {
-    print("Sending confirmation for order $orderId");
+    print("Отправка информации о заказе с идентификатором $orderId");
   }
 
   void _logOrderCreation() {
-    print("Logging order $orderId creation");
+    print("Создание события о создании заказа с идентификатором $orderId");
   }
 }
