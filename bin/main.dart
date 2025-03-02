fb397b5b00a5411d13f448657e247a2fb36f5b29
Ершов Андрей <rutikeyone@gmail.com>
2025-03-02 19:28:05 +0300

    The example has been completed

--- bin/main.dart
+++ bin/main.dart
@@ -1,21 +1,45 @@
-// Низкоуровневый модуль
-class Tank {
+// Абстракция (интерфейс)
+abstract class WaterSource {
+  void getWater();
+}
+
+// Низкоуровневый модуль для бака
+class Tank implements WaterSource {
+  @override
   void getWater() {
     print("Water from the tank");
   }
 }
 
+// Низкоуровневый модуль для водопровода
+class WaterPipe implements WaterSource {
+  @override
+  void getWater() {
+    print("Water from the water pipe");
+  }
+}
+
 // Высокоуровневый модуль
 class CoffeeMachine {
-  final Tank tank = Tank(); // Жёсткая зависимость от Tank
+  final WaterSource waterSource;
+
+  // Внедрение зависимости через конструктор
+  const CoffeeMachine(this.waterSource);
 
   void makeCoffee() {
-    tank.getWater();
+    waterSource.getWater();
     print("Coffee is ready!");
   }
 }
 
 void main() {
-  final coffeeMachine = CoffeeMachine();
-  coffeeMachine.makeCoffee();
+  // Используем Tank
+  final WaterSource tank = Tank();
+  final coffeeMachineWithTank = CoffeeMachine(tank);
+  coffeeMachineWithTank.makeCoffee();
+
+  // Используем WaterPipe
+  final WaterSource waterPipe = WaterPipe();
+  final coffeeMachineWithPipe = CoffeeMachine(waterPipe);
+  coffeeMachineWithPipe.makeCoffee();
 }
